322c86136683dc44cefe0c2b46f7bfa2332cb377
yaphott <londowski@proton.me>
2025-12-31 14:03:10 -0600

    test(builder.json_.string): add tests for `JSONStringLiteral`

---
+++ tests/builder/json_/test_string.py
@@ -0,0 +1,101 @@
+import pytest
+
+from grammatica.builder.json_.string import JSONString, JSONStringLiteral
+from grammatica.grammar.group.or_ import Or
+from grammatica.grammar.string import String
+
+try:
+    from ...helpers import fmt_result
+except ImportError:
+    import sys
+    from os import path
+
+    sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+    from tests.helpers import fmt_result
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        {
+            "description": "Empty",
+            "value": "",
+            "ensure_ascii": False,
+            "expected": String('""'),
+        },
+        {
+            "description": "ASCII characters",
+            "value": "gandalf",
+            "ensure_ascii": False,
+            "expected": String('"gandalf"'),
+        },
+        {
+            "description": "Non-ASCII characters with ensure_ascii=True",
+            "value": "gandalf😀",
+            "ensure_ascii": True,
+            "expected": String('"gandalf\\ud83d\\ude00"'),
+        },
+        {
+            "description": "Non-ASCII characters with ensure_ascii=False",
+            "value": "gandalf😀",
+            "ensure_ascii": False,
+            "expected": String('"gandalf😀"'),
+        },
+    ],
+)
+def test_json_string_literal_grammar(test_case):
+    component = JSONStringLiteral(test_case["value"], ensure_ascii=test_case["ensure_ascii"])
+    actual = component.grammar()
+    assert actual == test_case["expected"], "\n".join(
+        (
+            f"Description: {test_case['description']!r}",
+            f"Value: {fmt_result(test_case['value'])!s}",
+            f"Expected: {fmt_result(test_case['expected'])!s}",
+            f"Actual: {fmt_result(actual)!s}",
+        )
+    )
+
+
+def test_json_string_literal_attrs_dict():
+    component = JSONStringLiteral("gandalf😀", ensure_ascii=False)
+    expected = {"value": "gandalf😀", "ensure_ascii": False}
+    actual = component.attrs_dict()
+    assert actual == expected, "\n".join(
+        (
+            f"Component: {fmt_result(component)!s}",
+            f"Expected: {fmt_result(expected)!s}",
+            f"Actual: {fmt_result(actual)!s}",
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        {
+            "description": "Empty",
+            "component": JSONStringLiteral(""),
+            "expected": "JSONStringLiteral(value='', ensure_ascii=True)",
+        },
+        {
+            "description": "ASCII characters",
+            "component": JSONStringLiteral("gandalf"),
+            "expected": "JSONStringLiteral(value='gandalf', ensure_ascii=True)",
+        },
+        {
+            "description": "Non-ASCII characters",
+            "component": JSONStringLiteral("gandalf😀", ensure_ascii=False),
+            "expected": "JSONStringLiteral(value='gandalf😀', ensure_ascii=False)",
+        },
+    ],
+)
+def test_json_string_literal_as_string(test_case):
+    actual = test_case["component"].as_string(indent=None)
+    assert actual == test_case["expected"], "\n".join(
+        (
+            f"Description: {test_case['description']!r}",
+            f"Component: {fmt_result(test_case['component'])!s}",
+            f"Expected: {fmt_result(test_case['expected'])!s}",
+            f"Actual: {fmt_result(actual)!s}",
+        )
+    )
