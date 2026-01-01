4037450a25f28711c0e41e0b7cd473c5d66784c0
yaphott <londowski@proton.me>
2026-01-01 05:48:49 -0600

    test: add value of `ensure_ascii` in output of `JSONStringLiteral` test cases

--- tests/builder/json_/test_string.py
+++ tests/builder/json_/test_string.py
@@ -50,6 +50,7 @@ def test_json_string_literal_grammar(test_case):
         (
             f"Description: {test_case['description']!r}",
             f"Value: {fmt_result(test_case['value'])!s}",
+            f"Ensure ASCII: {fmt_result(test_case['ensure_ascii'])!s}",
             f"Expected: {fmt_result(test_case['expected'])!s}",
             f"Actual: {fmt_result(actual)!s}",
         )
@@ -99,3 +100,42 @@ def test_json_string_literal_as_string(test_case):
             f"Actual: {fmt_result(actual)!s}",
         )
     )
+
+
+# def test_json_boolean_grammar():
+#     component = JSONBoolean()
+#     actual = component.grammar()
+#     expected = Or([String("true"), String("false")])
+#     assert actual == expected, "\n".join(
+#         (
+#             f"Component: {fmt_result(component)!s}",
+#             f"Expected: {fmt_result(expected)!s}",
+#             f"Actual: {fmt_result(actual)!s}",
+#         )
+#     )
+#
+#
+# def test_json_boolean_attrs_dict():
+#     component = JSONBoolean()
+#     actual = component.attrs_dict()
+#     expected = {}
+#     assert actual == expected, "\n".join(
+#         (
+#             f"Component: {fmt_result(component)!s}",
+#             f"Expected: {fmt_result(expected)!s}",
+#             f"Actual: {fmt_result(actual)!s}",
+#         )
+#     )
+#
+#
+# def test_json_boolean_as_string():
+#     component = JSONBoolean()
+#     actual = component.as_string(indent=None)
+#     expected = "JSONBoolean()"
+#     assert actual == expected, "\n".join(
+#         (
+#             f"Component: {fmt_result(component)!s}",
+#             f"Expected: {fmt_result(expected)!s}",
+#             f"Actual: {fmt_result(actual)!s}",
+#         )
+#     )
